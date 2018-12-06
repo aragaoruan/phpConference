2fab40a2448eca61c9cf9d8ffb0dc40f4ffd1d4c
Ruan Aragao <aragao.ruan@gmail.com>
2018-12-06 14:38:49 -0200

    Table clientes

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Clientes;
 use Illuminate\Http\Request;
 
 class ClientesController extends Controller
 {
     public function index(){
         $view = view('clientes.index');
+
+        $clientes =  Clientes::all();
+
+        $view->clientes = $clientes;
+
+//        return $view->with('clientes', $clientes);
+
         return $view;
     }
 }
--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -2,5 +2,25 @@
 @extends('layout.app')
 
 @section('content')
-    aaa
+    <table>
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Nome</td>
+                <td>Telefone</td>
+                <td>CPF</td>
+            </tr>
+        </thead>
+        @foreach( $clientes as $cliente)
+            <tbody>
+                <td>{!! $cliente->id !!}</td>
+                <td>{!! $cliente->name !!}</td>
+                <td>{!! $cliente->telefone !!}</td>
+                <td>{!! $cliente->cpf !!}</td>
+            </tbody>
+
+        @endforeach
+
+    </table>
+
 @stop

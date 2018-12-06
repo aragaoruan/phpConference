02c815633cb160c2021e63e22984d2a81d51aab9
Ruan Aragao <aragao.ruan@gmail.com>
2018-12-06 15:27:50 -0200

    Componet input de text

---
+++ app/Providers/FormServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \Form::component(
+            'cText',
+            'components.text',
+            [
+                'name',
+                'label',
+                'value' => null,
+                'attributes' => []
+            ]
+        );
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
